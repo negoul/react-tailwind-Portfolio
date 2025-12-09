64a6fd18caace60e456baa918b1fdd49792675ef
negoul <negin.bagherzade@gmail.com>
2025-12-09 14:16:11 +0330

    add changes for Page github

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "./components/ui/toaster";
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: '/tailwind-react-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
