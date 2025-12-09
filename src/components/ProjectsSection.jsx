11a550f7ce28f5b26e16bb3a9d7e595ea4dfa449
negoul <negin.bagherzade@gmail.com>
2025-12-09 12:16:47 +0330

    Add version 1

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,33 @@
+
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+
+
+export const ProjectsSection = () => {
+  return(
+      <section id="projects" className="py-24 px-4 relative">
+       <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          {" "}
+          Featured <span className="text-primary"> Projects </span>
+        </h2>
+        
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Each project was carefully
+          crafted with attention to detail, performance, and user experience.
+        </p>
+       
+
+ <div className="text-center mt-12">
+          <a
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+            target="_blank"
+            href="https://github.com/machadop1407"
+          >
+            Check My Github <ArrowRight size={16} />
+          </a>
+        </div>
+
+        </div>
+    </section>
+  )
+}
